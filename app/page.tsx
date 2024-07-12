447d08f67592b0bc19fa10fce3ae043f2a43fd1a
taeyuls <startez03@gmail.com>
2024-07-13 02:38:06 +0900

    2일차

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  let name = "park";
+  return (
+    <div>
+      <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/list">List</Link>
+      </div>
+      <h4 className="title" style={{ color: "red", fontSize: "30px" }}>
+        애플후레시
+      </h4>
+      <p className="title-sub">by dev {name}</p>
+    </div>
+  );
 }
